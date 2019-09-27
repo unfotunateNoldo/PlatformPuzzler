b5be2906dfe45032a1a694574a384a9b6967ccd8
unfotunateNoldo <44146662+unfotunateNoldo@users.noreply.github.com>
2019-09-27 16:10:16 -0500

    Base states for grapple hook

---
+++ The Game/Assets/Scripts/PlayerScripts/GrappleHook.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleHook : Equipment {
+    private float cTime = 0;
+    public float minTimeToPreciseGrapple = 1.0f;
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.C)) {
+            cTime += Time.deltaTime;
+            if(cTime >= minTimeToPreciseGrapple) {
+                Debug.Log("Put precise grapple code here");
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.C)) {
+            if (cTime < minTimeToPreciseGrapple) {
+                Debug.Log("Put Quick Grapple code here");
+            }
+            cTime = 0;
+        }
+    }
+}
